391c036838dea92b6ce1efc71ef8dd81178fe0eb
moradi1993 <mohammad.moradi6030@gmail.com>
2025-12-13 02:21:42 +0330

    Update settings.py

--- digikala/settings.py
+++ digikala/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'shop', 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "shop" / "static",
 ]
 #STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, "shop", "static") ,
