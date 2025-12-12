d6ef0a29f000a3ea477fea9693e07e8a804daa9a
moradi1993 <mohammad.moradi6030@gmail.com>
2025-12-13 00:53:51 +0330

    Update settings.py

--- digikala/settings.py
+++ digikala/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
     'default': dj_database_url.config( 
         # Replace this value with your local database's connection string.
         #default='postgresql://postgres:postgres@localhost:5432/digikala',
-        default=os.environ.get('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
 }
