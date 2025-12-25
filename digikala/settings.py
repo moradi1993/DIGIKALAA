0822a55531e1e58330cf7e3a6a81bcaf8a44c788
moradi1993 <mohammad.moradi6030@gmail.com>
2025-12-25 18:07:15 +0330

    Update settings.py

--- digikala/settings.py
+++ digikala/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-vq5(p-9z3*xop%bj7%i#cjekq!^j23wmfg%i*chid*u2zg!y(o
 #DEBUG = True
 DEBUG = os.environ.git('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['digikalaa-1.onrender.com', 'mycustomdomin.ir']
+ALLOWED_HOSTS = ['digikalaa-1.onrender.com']
 
 #ALLOWED_HOSTS = ['https://digikalaa-1.onrender.com']
 
